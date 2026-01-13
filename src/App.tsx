7aecb214548444410d037d21923cbe82aafd7918
SIC_04_ELWAS <107783540+Elwass@users.noreply.github.com>
2026-01-13 22:42:44 +0700

    Allow scaled layout to extend height

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import LandingPage from "./pages/LandingPage";
+
+const App = () => {
+  return (
+    <div className="ui-scale">
+      <LandingPage />
+    </div>
+  );
+};
+
+export default App;
